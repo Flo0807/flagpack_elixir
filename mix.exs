d1aa999574c98da17a7ffe816f9e88df63ef6401
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2024-12-29 11:11:55 +0100

    Bump version to 0.5.1 in mix.exs

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Flagpack.MixProject do
   def project do
     [
       app: :flagpack,
-      version: "0.5.0",
+      version: "0.5.1",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
